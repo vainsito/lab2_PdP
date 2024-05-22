e6b7c88e0e6ea745b0ed6b46a4ac6b92340395d5
Ezequiel Marin <ezequiel.marin@mi.unc.edu.ar>
2024-05-22 18:25:50 -0300

    Agregamos atributo de repeticiones para las estadisticas

--- src/namedEntities/NamedEntity.java
+++ src/namedEntities/NamedEntity.java
@@ -1,23 +1,30 @@
 package namedEntities;
 import java.util.List;
 import java.util.ArrayList;
-
 public class NamedEntity {
+
     private String entidad_nombrada;
     private Category category;
     private List<Topics> topics;
+    private int repetitions;
 
     // Constructor 
     public NamedEntity(Category category, String palabra) {
         this.category = category;
         this.topics = new ArrayList<>();
         this.entidad_nombrada = palabra;
+        this.repetitions = 1;
     }
 
     // Setter
     public void addTopic(Topics topic) {
         this.topics.add(topic);
     }
+    
+    public void incrementRepetitions() {
+        this.repetitions++;
+    }
+    
     // Getters
     public Category getCategory() {
         return category;
@@ -27,12 +34,18 @@ public class NamedEntity {
         return topics;
     }
 
+    public int getRepetitions() {
+        return this.repetitions;
+    }
+
     // Metodo para imprimir
     public void namedEntityPrint(){
         System.out.println("Named Entity: " + this.entidad_nombrada);
         this.category.categoriePrint();
+        System.out.println("Repeticiones : "+ this.repetitions);
         for (Topics topic : this.topics){
             topic.topicPrint();
         }
+        System.out.println("-".repeat(80));
     }
 }
