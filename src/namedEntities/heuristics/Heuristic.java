e10fde72bc7ca7fdb98f577cc723b48adf02ddf0
Ezequiel Marin <ezequiel.marin@mi.unc.edu.ar>
2024-05-27 20:48:34 -0300

    Creo la interfaz Heuristic para poder agrupar las demas heuristicas y reutilizar codigo

---
+++ src/namedEntities/heuristics/Heuristic.java
@@ -0,0 +1,9 @@
+package namedEntities.heuristics;
+
+import java.util.List;
+// Creo esta interfaz para que las clases que implementen esta interfaz tengan que implementar el metodo extractCandidates
+// Esto me va a permitir modularizar el codigo, y no tener la necedidad de repetir el codigo
+// en funcion de la heuristica que se quiera emplear
+public interface Heuristic {
+    List<String> extractCandidates(String text);
+}
